d8a60a1cab9d70db4358aade7a8e2181b36acbe6
O.V.Sergeev <o.v.sergeyev@gmail.com>
2025-08-01 09:41:26 +0300

    delete bot

--- bot/core/handlers/common.py
+++
@@ -1,7 +0,0 @@
-from aiogram.types import CallbackQuery
-
-
-async def delete_post(callback: CallbackQuery):
-    chat_id = callback.message.chat.id
-    message_id = callback.message.message_id
-    await callback.bot.delete_message(chat_id=chat_id, message_id=message_id)
